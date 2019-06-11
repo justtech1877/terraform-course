d6a15e4ee15da2da72f8e482166f08067ca4af54
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:18:50 +0100

    Initial commit

--- demo-6/modules.tf
+++ demo-6/modules.tf
@@ -1,10 +1,10 @@
 module "consul" {
   source = "github.com/wardviaene/terraform-consul-module.git"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  key_name = "${aws_key_pair.mygenkey.key_name}"
   key_path = "${var.PATH_TO_PRIVATE_KEY}"
   region = "${var.AWS_REGION}"
   vpc_id = "${aws_default_vpc.default.id}"
-  subnets {
+  subnets = {
     "0" = "${aws_default_subnet.default_az1.id}"
     "1" = "${aws_default_subnet.default_az2.id}"
     "2" = "${aws_default_subnet.default_az3.id}"
