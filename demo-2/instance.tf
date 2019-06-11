7ac898fab1c730c458aaaba21f6637b7a2121a21
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:15:47 +0100

    Initial commit

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -1,12 +1,15 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
+resource "aws_key_pair" "mygenkey" {
+  key_name = "mygenkey"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "khefkh" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  key_name = "${aws_key_pair.mygenkey.key_name}"
+  subnet_id = "subnet-03a8c8d5250f6691a"
+  vpc_security_group_ids = [
+              "sg-06d19beef288595d4"]
 
   provisioner "file" {
     source = "script.sh"
@@ -19,6 +22,7 @@ resource "aws_instance" "example" {
     ]
   }
   connection {
+    host = "${self.public_ip}"
     user = "${var.INSTANCE_USERNAME}"
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
   }
