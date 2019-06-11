43c81ffbc3c306db02c264275bad88ecc81c9af0
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:49:29 +0100

    Initial commit

--- packer-demo/provider.tf
+++ packer-demo/provider.tf
@@ -1,3 +1,7 @@
-provider "aws" { 
+provider "aws" {
+ #   access_key = "${var.AWS_ACCESS_KEY}"
+ #   secret_key = "${var.AWS_SECRET_KEY}"
+    profile = "${var.PROFILE}"
     region = "${var.AWS_REGION}"
 }
+
--- packer-demo/vars.tf
+++ packer-demo/vars.tf
@@ -1,9 +1,12 @@
+variable "PROFILE" {}
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-west-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "mygenkey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "mygenkey.pub"
 }
