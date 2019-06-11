0bee94c8af7fdc3711bcadd3ddb2695e5f0efffd
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:35:02 +0100

    Initial commit

--- demo-15/autoscaling.tf
+++ demo-15/autoscaling.tf
@@ -1,15 +1,15 @@
-resource "aws_launch_configuration" "example-launchconfig" {
-  name_prefix          = "example-launchconfig"
+resource "aws_launch_configuration" "tf-demo15-launchconfig" {
+  name_prefix          = "tf-demo15-launchconfig"
   image_id             = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type        = "t2.micro"
-  key_name             = "${aws_key_pair.mykeypair.key_name}"
-  security_groups      = ["${aws_security_group.allow-ssh.id}"]
+  instance_type        = "t3.micro"
+  key_name             = "${aws_key_pair.mygenkey.key_name}"
+  security_groups      = ["${aws_security_group.tf-demo15-instance.id}"]
 }
 
-resource "aws_autoscaling_group" "example-autoscaling" {
-  name                 = "example-autoscaling"
-  vpc_zone_identifier  = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"]
-  launch_configuration = "${aws_launch_configuration.example-launchconfig.name}"
+resource "aws_autoscaling_group" "tf-demo15-autoscaling" {
+  name                 = "tf-demo15-autoscaling"
+  vpc_zone_identifier  = ["${aws_subnet.stage-vpc-public-1.id}", "${aws_subnet.stage-vpc-public-2.id}"]
+  launch_configuration = "${aws_launch_configuration.tf-demo15-launchconfig.name}"
   min_size             = 1
   max_size             = 2
   health_check_grace_period = 300
@@ -18,7 +18,7 @@ resource "aws_autoscaling_group" "example-autoscaling" {
 
   tag {
       key = "Name"
-      value = "ec2 instance"
+      value = "tf-demo15 instance"
       propagate_at_launch = true
   }
 }
--- demo-15/sns.tf
+++ demo-15/sns.tf
@@ -1,13 +1,13 @@
 # Uncomment if you want to have autoscaling notifications
 
-#resource "aws_sns_topic" "example-sns" {
-#  name         = "sg-sns"
-#  display_name = "example ASG SNS topic"
+#resource "aws_sns_topic" "asg-sns" {
+#  name         = "asg-sns"
+#  display_name = "ASG SNS topic"
 #} # email subscription is currently unsupported in terraform and can be done using the AWS Web Console
 #
-#resource "aws_autoscaling_notification" "example-notify" {
-#  group_names = ["${aws_autoscaling_group.example-autoscaling.name}"]
-#  topic_arn     = "${aws_sns_topic.example-sns.arn}"
+#resource "aws_autoscaling_notification" "asg-notify" {
+#  group_names = ["${aws_autoscaling_group.tf-demo15-autoscaling.name}"]
+#  topic_arn     = "${aws_sns_topic.asg-sns.arn}"
 #  notifications  = [
 #    "autoscaling:EC2_INSTANCE_LAUNCH",
 #    "autoscaling:EC2_INSTANCE_TERMINATE",
