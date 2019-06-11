fae87038d8c462751fe65729dbf61ecbd09523d1
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:46:37 +0100

    Initial commit

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,14 @@
+
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+profile = "cr-labs-skill-up"
+region = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+
+resource "aws_instance" "test1" {
+  ami           = "ami-0009a33f033d8b7b6"
   instance_type = "t2.micro"
+  subnet_id = "subnet-03a8c8d5250f6691a"
+  vpc_security_group_ids = [
+              "sg-06d19beef288595d4"]
 }
