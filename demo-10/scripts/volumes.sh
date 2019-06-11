5594382bf2456013f7469cef35a7c65b0babbbcf
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:27:00 +0100

    Initial commit

--- demo-10/scripts/volumes.sh
+++ demo-10/scripts/volumes.sh
@@ -4,8 +4,8 @@ set -ex
 
 vgchange -ay
 
-DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
-if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
+DEVICE_FS=`blkid -o value -s TYPE ${DEVICE} || echo ""`
+if [ "`echo -n $DEVICE_FS`" == "" ]; then 
   # wait for the device to be attached
   DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
   DEVICEEXISTS=''
@@ -26,4 +26,17 @@ echo '/dev/data/volume1 /data ext4 defaults 0 0' >> /etc/fstab
 mount /data
 
 # install docker
-curl https://get.docker.com | bash
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo chkconfig docker on
+sudo yum install -y git
+#Copy the appropriate docker-compose binary from GitHub:
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+# install docker
+#curl https://get.docker.com | bash  # for ubuntu
+
+
--- demo-10/vars.tf
+++ demo-10/vars.tf
@@ -1,19 +1,26 @@
+variable "PROFILE" {}
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
-}
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "eu-west-2"
 }
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-2 = "ami-0009a33f033d8b7b6"
+    eu-west-1 = "ami-0cbf7a0c36bde57c9"
   }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mygenkey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mygenkey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
 }
 variable "INSTANCE_DEVICE_NAME" {
   default = "/dev/xvdh"
