842439b6f8b40af111637852e8892aa9a7cb66b4
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-11 14:13:40 +0100

    Initial commit

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,13 +1,14 @@
+variable "PROFILE" {}
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-west-2"
 }
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    eu-west-2 = "ami-0009a33f033d8b7b6"
+    eu-west-1 = "ami-08eb05c142193af92"
   }
 }
