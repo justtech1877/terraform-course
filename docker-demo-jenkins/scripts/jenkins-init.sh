5f8a0abebc0811e48e1a45b3406b18a61d63f31c
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-14 12:08:50 +0100

    Update jenkins-init.sh file

--- docker-demo-jenkins/scripts/jenkins-init.sh
+++ docker-demo-jenkins/scripts/jenkins-init.sh
@@ -51,7 +51,7 @@ sudo yum install -y git
 # install docker 
 sudo amazon-linux-extras install docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker jenkins
 sudo chkconfig docker on
 
 
